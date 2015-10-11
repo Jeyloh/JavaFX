2f741c7b11b5e72b9e57deb07ac8a9c25445e860
Jeyloh <jlh.94@hotmail.com>
2015-10-11 21:22:23 +0200

    Basic Structure of a JavaFX

--- Intro_to_Java_Programming_10th/src/chapter_14_javaFX/firstClass.java
+++ Intro_to_Java_Programming_10th/src/chapter_14_javaFX/firstClass.java
@@ -13,21 +13,52 @@ import javafx.stage.Stage;
 
 
 /**
- *
- * @author Tyrion
+ * 
+ * 
+ * How do you define a JavaFX main class? 
+ *      public static void main(args[]) ** false
+ *      public static void main(String[] args) ** correct
+ * What is the signature of the start method? 
+ *      public void start(Stage stageName)
+ * What is a stage? 
+ *      The main window for an application where you put all the nodes
+ * What is a primary stage? 
+ *      An object of Stage 
+ * Is a primary stage automatically created? 
+ *      Yes, in the signature of the start method. 
+ * How do you display a stage? 
+ *      stageName.show()
+ * Can you prevent the user from resizing the stage? 
+ *      stageName.resizable(false)
+ * Can you replace Application.launch(args)by launch(args) in line 22 in Listing 14.1?
+ *      Yes, because ClassName extends Application
+ * 
+ * 
+ * 
+ * @author jeyloh
  */
-public class firstClass extends Application{
-    @Override
+public class FirstClass extends Application{
+    @Override //Run start() and create a Stage object named primaryStage
     public void start(Stage primaryStage) {
-        Button btOK = new Button("OK");
-        //Constructor Scene(node, width, height)
-        Scene scene = new Scene(btOK, 200, 250);
+        //Create a scene and place a button in the scene
+        //The scene object has 3 paramters, Scene(node/button, width, height)
+        Scene scene = new Scene(new Button("OK"), 200, 250);
+        //Imagine the stage as a platform to support scenes and nodes as actors to perform in the scenes
         primaryStage.setTitle("MyJavaFX");
         primaryStage.setScene(scene);
         primaryStage.show();
+        
+        //Create the second stage with new values, both will show at runtime
+        Stage secondStage = new Stage(); //new stage object
+        secondStage.setTitle("Second Stage"); //decide stage title
+        //Set a scene with a button in the stage
+        secondStage.setScene(new Scene(new Button("New Stage"), 100, 100));
+        secondStage.setResizable(false); //cant resize the second window
+        secondStage.show();
     }
     
     //main is needed if you're not launching from command line 
+    //this is always the same for any JavaFX source code
     public static void main(String[] args) {
         //launch is a method from Application used to launch JavaFX applications
         Application.launch(args);
